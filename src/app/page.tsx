d970c387f310b45522c2cde8c3cbf5027667f4f7
thevgseven <ivgseven@gmail.com>
2024-01-09 16:35:38 +0530

    v3.0.0: home-screen patches fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { cn } from "@/lib/utils/cn";
 import { PatriotFont } from "@/ui/fonts/fonts";
 import { Canvas } from "@/ui/components/canvas";
+import { Button } from "@/ui/primitives/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h1
         className={cn(
-          "fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-6xl md:text-8xl text-center text-white",
+          "fixed top-1/2 max-sm:top-72 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-6xl md:text-8xl text-center text-white",
           PatriotFont.className
         )}
       >
@@ -19,13 +21,22 @@ export default function Home() {
         </div>
       </h1>
       <h1
-        className={cn(
-          "fixed top-3/4 left-1/2 -translate-x-1/2 -translate-y-1/2 text-lg text-center text-white"
-        )}
+        className={
+          "fixed top-3/4 max-sm:top-2/3 left-1/2 -translate-x-1/2 -translate-y-1/2 text-lg text-center text-white"
+        }
       >
         Obsessed with AI, Quantum Computing, Space, Psychology and Philosophy
         and Entrepreneurship..
       </h1>
+      <Link href={"/projects"}>
+        <Button
+          className={
+            "fixed mt-14 top-3/4 left-1/2 -translate-x-1/2 -translate-y-1/2 text-lg"
+          }
+        >
+          Explore
+        </Button>
+      </Link>
       <Canvas />
     </div>
   );
